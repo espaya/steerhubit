dfbfdc728c80690f9f1449ab0a70e42161fb8cc6
espaya <beehivelive8@gmail.com>
2025-03-18 03:06:52 +0000

    Added logout button

--- resources/views/employer/employer_temp/header.blade.php
+++ resources/views/employer/employer_temp/header.blade.php
@@ -116,7 +116,11 @@
                                     <li><a class="dropdown-item" href="{{ route('employer.candidate.shortlist') }}">Candidate Shortlist</a></li>
                                     <li><a class="dropdown-item" href="{{ route('employer.package') }}">Subscription</a></li>
                                     <li><a class="dropdown-item" href="{{ route('employer.change.password') }}">Change Password</a></li>
-                                    <li><a class="dropdown-item" href="#">Log Out</a></li>
+                                    <li><a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log Out</a>
+                                    </li>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
                                     <li><a class="dropdown-item" href="{{ route('employer.delete.profile') }}">Delete Account</a></li>
                                 </ul>  
                             </div>
