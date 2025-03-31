78e4c7be21d1c38f0f11d709674652769bcf7cc9
espaya <beehivelive8@gmail.com>
2025-03-31 14:01:36 +0000

    Added employers

--- app/Http/Controllers/Management/ManagementEmployersController.php
+++ app/Http/Controllers/Management/ManagementEmployersController.php
@@ -3,12 +3,44 @@
 namespace App\Http\Controllers\Management;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ManagementEmployersController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.employers');
+        $perPage = $request->query('per_page', 10); // Default to 10 if not set
+        $search = trim($request->query('search', ''));
+
+        $query = User::where('role', 'EMPLOYER')->orderBy('id', 'DESC'); 
+
+        if (!empty($search)) 
+        {
+            $query->where(function ($q) use ($search) 
+            {
+                $q->where('name', 'LIKE', "%$search%")
+                ->orWhere('email', 'LIKE', "%$search%");
+            });
+        }
+
+        $employees = $query->paginate($perPage);
+        $totalEmployees = User::where('role', 'Candidate')->count();
+
+        $limits = [];
+
+        for ($i = 10; $i <= $totalEmployees; $i *= 2) 
+        {
+            $limits[] = $i;
+        }
+
+        // Ensure the last option is the total number of employees
+        if ($totalEmployees > end($limits)) 
+        {
+            $limits[] = $totalEmployees;
+        }
+
+        return view('admin.employers', compact('employees', 'totalEmployees', 'limits'));
+    
     }
 }
