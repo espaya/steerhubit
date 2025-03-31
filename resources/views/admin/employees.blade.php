62125c909cf2c9a27ef3ced06120ba3885a3d7b9
espaya <beehivelive8@gmail.com>
2025-03-31 12:09:07 +0000

    Fixed title

--- resources/views/admin/employees.blade.php
+++ resources/views/admin/employees.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>StrikingDash</title>
+    <title>Employees - SteerHubIt</title>
 
     <link href="../../../../css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
