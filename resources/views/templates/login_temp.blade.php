fdaac3067e2a72017544f83466061c72d3e298e1
espaya <beehivelive8@gmail.com>
2025-05-09 00:50:23 +0000

    Removed recaptcha

--- resources/views/templates/login_temp.blade.php
+++ resources/views/templates/login_temp.blade.php
@@ -44,13 +44,6 @@
                      </div>
 
                      <div class="form-group my-3">
-                        {!! NoCaptcha::renderJs() !!}
-                        {!! NoCaptcha::display() !!} 
-                     </div> 
-                     
-                     <small id="login-error-recaptcha" class="text-danger"></small>
-
-                     <div class="form-group my-3">
                         <button id="login-button" type="submit" class="rts__btn w-100 fill__btn">Login</button>
                      </div>
                   </form>
