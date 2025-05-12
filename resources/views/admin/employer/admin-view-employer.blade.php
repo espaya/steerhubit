11ca89f9f557621992655ad90d70c1a18049f19d
espaya <beehivelive8@gmail.com>
2025-05-12 02:34:46 +0000

    Fixed applicants total

--- resources/views/admin/employer/admin-view-employer.blade.php
+++ resources/views/admin/employer/admin-view-employer.blade.php
@@ -220,7 +220,7 @@
                                                     <div>
 
                                                         <div class="overview-content">
-                                                            <h1> {{ (int)$countApplicants }} </h1>
+                                                            <h1> {{ $countApplicants ? (int)$countApplicants : '0' }} </h1>
                                                             <p>Applicants</p>
                                                             <div class="ap-po-details-time">
                                                                 <span class="color-danger"><i class="las la-arrow-down"></i>
