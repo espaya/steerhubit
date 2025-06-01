00ddaffe90ba809faa153709610246a9e750b228
espaya <beehivelive8@gmail.com>
2025-06-01 00:16:16 +0000

    Fixed meta info

--- resources/views/employee/employee.blade.php
+++ resources/views/employee/employee.blade.php
@@ -4,26 +4,26 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="apple-mobile-web-app-capable" content="yes">
-      <meta name="description" content="Your Ultimate Job HTML Template">
+      <meta name="description" content="Access your personalized employee dashboard on SteerHubIT to manage job applications, update your resume, and connect with top employers.">
+
       <meta name="keywords" content="Job, Resume, Employer, Agency">
-      <link rel="canonical" href="https://html.themewant.com/jobpath">
+      <link rel="canonical" href="{{ route('employee') }}">
       <meta name="robots" content="index, follow">
       <!-- for open graph social media -->
-      <meta property="og:title" content="Your Ultimate Job HTML Template">
-      <meta property="og:description" content="Your Ultimate Job HTML Template">
+      <meta property="og:title" content="Employee Dashboard - SteerHubIT">
+      <meta property="og:description" content="Access your personalized employee dashboard on SteerHubIT to manage job applications, update your resume, and connect with top employers.">
       <meta property="og:image" content="https://www.example.com/image.jpg">
-      <meta property="og:url" content="https://html.themewant.com/jobpath/">
+      <meta property="og:url" content="{{ route('employee') }}">
       <!-- for twitter sharing -->
       <meta name="twitter:card" content="summary_large_image">
-      <meta name="twitter:title" content="Your Ultimate Job HTML Template">
-      <meta name="twitter:description" content="Your Ultimate Job HTML Template">
+      <meta name="twitter:title" content="Employee Dashboard - SteerHubIT">
+      <meta name="twitter:description" content="Access your personalized employee dashboard on SteerHubIT to manage job applications, update your resume, and connect with top employers.">
       <!-- fabicon -->
       <link rel="shortcut-icon" href="{{asset('assets/img/favicon-16x16.png')}}" type="image/x-icon">
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
-      <link href="../../css2?family=Plus+Jakarta+Sans:wght@200..800&display=swap" rel="stylesheet">
       <link rel="shortcut icon" href="{{asset('assets/img/favicon.ico')}}" type="image/x-icon">
-      <title>SteerHubIT - Employee Dashboard</title>
+      <title>Employee Dashboard - SteerHubIT</title>
       <!-- rt icons -->
       <link rel="stylesheet" href="{{asset('assets/fonts/icon/css/rt-icons.css')}}">
       <!-- fontawesome -->
