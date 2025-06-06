4ea7fe846575803d7684390c2a676f2318c6dd1d
espaya <beehivelive8@gmail.com>
2025-06-06 02:35:57 +0000

    fixed avatar

--- resources/views/employee/employee-job-shortlisted.blade.php
+++ resources/views/employee/employee-job-shortlisted.blade.php
@@ -1,9 +1,9 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    
-<meta charset="UTF-8">
+
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="description" content="Your Ultimate Job HTML Template">
@@ -26,9 +26,8 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
-    <link href="../../css2?family=Plus+Jakarta+Sans:wght@200..800&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="{{asset('assets/img/favicon.ico')}}" type="image/x-icon">
-    <title>SteerHubIT - Job Shortlisted</title>
+    <title>Shortlisted Job(s) - SteerHubIT</title>
     <!-- rt icons -->
     <link rel="stylesheet" href="{{asset('assets/fonts/icon/css/rt-icons.css')}}">
     <!-- fontawesome -->
@@ -38,14 +37,15 @@
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 
 </head>
+
 <body class="template-dashboard">
     <!-- header area -->
     @include('employee/employee_temp/emp-header');
-<!-- header area end -->
-    
+    <!-- header area end -->
+
     <!-- content area -->
     <div class="dashboard__content d-flex">
-    @include('employee/employee_temp/emp-sidebar');
+        @include('employee/employee_temp/emp-sidebar');
         <div class="dashboard__right">
             <div class="dash__content ">
                 <!-- sidebar menu -->
@@ -53,7 +53,7 @@
                     <div class="sidebar__action"><i class="fa-sharp fa-regular fa-bars"></i> Sidebar</div>
                 </div>
                 <!-- sidebar menu end -->
-                 
+
                 <div class="applied__job__info radius-16">
                     <div class="job__filter">
                         <div class="search__job">
@@ -67,16 +67,18 @@
                     </div>
 
                     <div class="applied__job__list">
-                        
-                    @forelse($shortlisted as $shortlist)
+
+                        @forelse($shortlisted as $shortlist)
                         <!-- single job -->
                         <div class="single__applied__job">
-                            <div class="single__applied__job__content">   
+                            <div class="single__applied__job__content">
                                 <div class="icon">
-                                    <img src="{{asset('assets/img/icon/apple.svg')}}" alt="">
+                                    <img src="{{ $shortlist->job->user->avatar ? asset('uploads/avatars/' . $shortlist->job->user->avatar) : asset('assets/img/dashboard/profile.png')}}" alt="">
                                 </div>
                                 <div class="content">
-                                    <a href="{{ route('job.view', ['slug' => $shortlist->job->slug]) }}"> <h6> {{ $shortlist->job->title }} </h6></a>
+                                    <a href="{{ route('job.view', ['slug' => $shortlist->job->slug]) }}">
+                                        <h6> {{ $shortlist->job->title }} </h6>
+                                    </a>
                                     <div class="content__info">
                                         <span><i class="fa-light fa-location-dot"></i> {{ $shortlist->job->state . ', ' . $shortlist->job->country }}, </span>
                                         <span><i class="fa-light fa-briefcase"></i> {{ $shortlist->job->working_day }} </span>
@@ -94,40 +96,40 @@
                             </div>
                         </div>
                         <!--single job end  -->
-                    @empty 
+                        @empty
                         <div class="alert alert-info">No Jobs Found</div>
-                    @endforelse
+                        @endforelse
                     </div>
                     <!-- pagination -->
                     <div class="rts__pagination d-block mx-auto pt-40 max-content">
-                    @if ($shortlisted->hasPages())
+                        @if ($shortlisted->hasPages())
                         <ul class="d-flex gap-2">
                             {{-- Previous Page Link --}}
                             <li>
                                 @if ($shortlisted->onFirstPage())
-                                    <a class="inactive"><i class="rt-chevron-left"></i></a>
+                                <a class="inactive"><i class="rt-chevron-left"></i></a>
                                 @else
-                                    <a href="{{ $shortlisted->previousPageUrl() }}"><i class="rt-chevron-left"></i></a>
+                                <a href="{{ $shortlisted->previousPageUrl() }}"><i class="rt-chevron-left"></i></a>
                                 @endif
                             </li>
 
                             {{-- Pagination Elements --}}
                             @foreach ($shortlisted->links()->elements[0] as $page => $url)
-                                <li>
-                                    <a class="{{ $page == $shortlisted->currentPage() ? 'active' : '' }}" href="{{ $url }}">{{ $page }}</a>
-                                </li>
+                            <li>
+                                <a class="{{ $page == $shortlisted->currentPage() ? 'active' : '' }}" href="{{ $url }}">{{ $page }}</a>
+                            </li>
                             @endforeach
 
                             {{-- Next Page Link --}}
                             <li>
                                 @if ($shortlisted->hasMorePages())
-                                    <a href="{{ $shortlisted->nextPageUrl() }}"><i class="rt-chevron-right"></i></a>
+                                <a href="{{ $shortlisted->nextPageUrl() }}"><i class="rt-chevron-right"></i></a>
                                 @else
-                                    <a class="inactive"><i class="rt-chevron-right"></i></a>
+                                <a class="inactive"><i class="rt-chevron-right"></i></a>
                                 @endif
                             </li>
                         </ul>
-                    @endif
+                        @endif
 
                     </div>
                     <!-- pagination end -->
@@ -138,24 +140,25 @@
         </div>
     </div>
     <!-- content area end -->
-    
+
 
     @include('templates/offcanvas')
-<!-- THEME PRELOADER START -->
-<div class="loader-wrapper">
-    <div class="loader">
+    <!-- THEME PRELOADER START -->
+    <div class="loader-wrapper">
+        <div class="loader">
+        </div>
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
     </div>
-    <div class="loader-section section-left"></div>
-    <div class="loader-section section-right"></div>
-</div>
-<!-- THEME PRELOADER END -->
-<button type="button" class="rts__back__top" id="rts-back-to-top">
-    <i class="fas fa-arrow-up"></i>
-</button>
-<!-- all plugin js -->
-<script src="{{asset('assets/js/plugins.min.js')}}"></script>
-<script src="{{asset('assets/js/main.js')}}"></script>
-    
-    
+    <!-- THEME PRELOADER END -->
+    <button type="button" class="rts__back__top" id="rts-back-to-top">
+        <i class="fas fa-arrow-up"></i>
+    </button>
+    <!-- all plugin js -->
+    <script src="{{asset('assets/js/plugins.min.js')}}"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
+
+
 </body>
+
 </html>
